9060018cc7c290a7eeafe183326fe6d8ee6436d2
temattcha <89668544+temattcha@users.noreply.github.com>
2022-03-24 17:18:41 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,9 +15,9 @@
 
     // === logo change
     if (ud_header.classList.contains("sticky")) {
-      logo.src = "assets/images/logo/logo.png";
+      logo.src = "assets/images/logo/logo.webp";
     } else {
-      logo.src = "assets/images/logo/logo.png";
+      logo.src = "assets/images/logo/logo.webp";
     }
 
     // show or hide the back-top-top button
