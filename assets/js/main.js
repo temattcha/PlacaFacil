44b89434d6878f2c5c3de1f7e666126d8ba948e1
temattcha <89668544+temattcha@users.noreply.github.com>
2022-04-19 14:40:44 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,9 +15,9 @@
 
     // === logo change
     if (ud_header.classList.contains("sticky")) {
-      logo.src = "assets/images/logo/logo.webp";
+      logo.src = "assets/images/logo/logo-placa.png";
     } else {
-      logo.src = "assets/images/logo/logo.webp";
+      logo.src = "assets/images/logo/logo-placa.png";
     }
 
     // show or hide the back-top-top button
